c00f82c2a877b1263e96c6ea567a871ea820c855
nanda kishore <nk732100@gmail.com>
2021-07-19 09:08:39 +0530

    1. Messages are sent and received as JSON object instead as a string.

--- src/room_manager.py
+++ src/room_manager.py
@@ -3,9 +3,8 @@ from fastapi import FastAPI, WebSocket
 import random
 import string
 import json
-import status_codes
-import select
 from connection_manager import manager
+from utilities import MessageType, MessagePurpose, Message
 import logging
 
 
@@ -15,7 +14,7 @@ class RoomManager:
         self.active_room_codes = ['']
 
     def create_room(self, room_details, ws):
-        ''' 
+        '''
             room_details = {
                 'room_name': 'some name',
                 'owner_name': 'Nanda Kishore',
@@ -24,8 +23,8 @@ class RoomManager:
 
             ws: websocket connected to the owner of the room.
 
-            This method takes room_details, creates a room and returns the 6 digit 
-            alphanumeric code to the owner using which he can invite the people into 
+            This method takes room_details, creates a room and returns the 6 digit
+            alphanumeric code to the owner using which he can invite the people into
             the room.
         '''
 
@@ -37,9 +36,9 @@ class RoomManager:
         return room
 
     async def join_room(self, join_details, ws):
-        ''' 
+        '''
             This method takes the user details (join_details parameter) as input,
-            finds the room owner for the room the user wants to join and requests 
+            finds the room owner for the room the user wants to join and requests
             permission from the owner.
         '''
         room_code = join_details['room_code']
@@ -47,7 +46,9 @@ class RoomManager:
             room = self.active_rooms[room_code]
             await room.join_room(join_details, ws)
         except KeyError:
-            await ws.send_text("Invalid Room Code.")
+            message = Message(room_code, "Invalid Room Code.",
+                              "Room Manager", MessagePurpose.OTHERS)
+            await ws.send_json(message.as_json())
 
     async def add_user_to_room(self, room_code, user_details, ws):
         try:
@@ -78,17 +79,7 @@ class Room:
         self.room_members_details[self.room_owner_id] = {
             'user_id': self.room_owner_id, 'username': self.room_owner}
 
-    def disconnect(self, websocket: WebSocket):
-        self.room_members_sockets.remove(websocket)
-
-    async def send_message_to_owner(self, message):
-        print("trying to send_personal_message to owner")
-        await self.room_owner_ws.send_text(message)
-
-    async def send_personal_message(self, message: str, ws: WebSocket):
-        await ws.send_text(message)
-
-    async def broadcast_message(self, message, ws=None):
+    async def __broadcast_text_message(self, message, ws=None):
         if ws:
             for member_socket in self.room_members_sockets:
                 if member_socket != ws:
@@ -97,14 +88,59 @@ class Room:
             for member_socket in self.room_members_sockets:
                 await member_socket.send_text(message)
 
+    async def __broadcast_json_message(self, message, ws=None):
+        if ws:
+            for member_socket in self.room_members_sockets:
+                if member_socket != ws:
+                    await member_socket.send_json(message)
+        else:
+            for member_socket in self.room_members_sockets:
+                await member_socket.send_json(message)
+
+    async def __send_text_message_to_owner(self, message):
+        await self.room_owner_ws.send_text(message)
+
+    async def __send_json_message_to_owner(self, message):
+        await self.room_owner_ws.send_json(message)
+
+    async def __send_personal_text_message(self, message: str, ws: WebSocket):
+        await ws.send_text(message)
+
+    async def __send_personal_json_message(self, message: str, ws: WebSocket):
+        await ws.send_json(message)
+
+    def disconnect(self, websocket: WebSocket):
+        self.room_members_sockets.remove(websocket)
+
+    async def send_personal_message(self, message: str, ws: WebSocket, message_type=MessageType.JSON_MESSAGE):
+        if message_type is MessageType.TEXT_MESSAGE:
+            await self.__send_personal_text_message(message, ws)
+        else:
+            await self.__send_personal_json_message(message, ws)
+
+    async def send_message_to_owner(self, message, message_type=MessageType.JSON_MESSAGE):
+        print("trying to send_personal_message to owner")
+        if message_type is MessageType.TEXT_MESSAGE:
+            await self.__send_text_message_to_owner(message)
+        else:
+            await self.__send_json_message_to_owner(message)
+
+    async def broadcast_message(self, message, ws=None, message_type=MessageType.JSON_MESSAGE):
+        if message_type is MessageType.TEXT_MESSAGE:
+            await self.__broadcast_text_message(message, ws=ws)
+        else:
+            await self.__broadcast_json_message(message, ws=ws)
+
     async def join_room(self, member_details, ws):
         print("control inside Room.join_room::", member_details)
         request_string = f"<meta>room_join_request</meta>::{member_details['user_id']}::"
         request_string += f"{member_details['username']} Requested To Join Your Room."
+        message = Message(self.room_code, request_string,
+                          {'user_id': member_details['user_id'], 'username': member_details['username']}, MessagePurpose.JOIN_REQUEST)
+        await self.send_message_to_owner(message.as_json())
 
-        await self.send_message_to_owner(request_string)
         while True:
-            member_message = await ws.receive_text()
+            member_message = await ws.receive_json()
             print("member_message::", member_message)
 
             await self.broadcast_message(member_message, ws)
@@ -113,12 +149,20 @@ class Room:
         print("userDetial sin 89:", user_details)
         self.room_members_sockets.append(ws)
         self.room_members_details[user_details['user_id']] = user_details
-        await self.send_personal_message("<meta>request_accepted</meta>::200", ws)
-        await self.broadcast_message(f"{user_details['username']} Has Entered The Chat.")
+
+        message = Message(
+            self.room_code, "<meta>request_accepted</meta>::200", {}, MessagePurpose.ACCEPT_REQUEST)
+        await self.send_personal_message(message.as_json(), ws)
+
+        message = Message(self.room_code, f"{user_details['username']} Has Entered The Chat.", {
+        }, MessagePurpose.NEW_MEMBER_JOINED)
+        await self.broadcast_message(message.as_json())
 
     async def listen_for_joiners(self):
         while True:
-            message = await self.room_owner_ws.receive_text()
+            response = await self.room_owner_ws.receive_json()
+            message = response['content']
+
             accept_string = "<meta>accept_request</meta>::"
             deny_string = "<meta>deny_request</meta>::"
 
@@ -135,6 +179,6 @@ class Room:
 
                 user = manager.active_members[user_id]
                 manager.send_personal_message(
-                    f"Owner Of The Room {room_id} Has Not Accepted Your Request.", user)
+                    f"Owner Of The Room {self.room_name} Has Not Accepted Your Request.", user)
             else:
-                await self.broadcast_message(message, self.room_owner_ws)
+                await self.broadcast_message(response, self.room_owner_ws)
